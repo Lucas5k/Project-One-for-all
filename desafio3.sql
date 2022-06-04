686bd17c6d5f204e8917287f9b680de0a6e1680c
Lucas5k <lucascabral112347@gmail.com>
2022-06-04 16:10:38 -0300

    REQ 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,9 @@
-SELECT name AS usuario, COUNT(*) AS qtde_musicas_ouvidas
+SELECT name AS usuario, COUNT(*) AS qtde_musicas_ouvidas, ROUND(SUM((duration) / 60), 2) AS total_minutos
 FROM
     users
         INNER JOIN
     reproduction_history ON users.id = reproduction_history.users_id
-GROUP BY usuario;
+        INNER JOIN 
+    songs ON reproduction_history.song_id = songs.id
+GROUP BY usuario
+ORDER BY usuario ASC;
